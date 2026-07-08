10bb1760eb10ad6ad80d0a8da655534afc2ca99a
ImZacvin <cdarmajaya@gmail.com>
2026-07-08 16:26:16 +0700

    Add axios api frontend

--- frontend/src/api/applicant.api.js
+++ frontend/src/api/applicant.api.js
@@ -1,3 +1,4 @@
 import api from './axios';
 
-export const getAll = () => api.get('/applicant'); 
+export const getAll = () => api.get('/applicant');
+export const getAllByCompany = (job_id) => api.get(`/applicant/company/${job_id}`);
