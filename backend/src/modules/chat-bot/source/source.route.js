6c7ddb44bfd1f48d0c8249e48acb1cf82dc2a51b
Muhammad Luthfi Naufal <mluthfinaufal99@gmail.com>
2026-09-08 16:05:16 +0700

    fix dependency

--- backend/src/modules/chat-bot/source/source.route.js
+++ backend/src/modules/chat-bot/source/source.route.js
@@ -2,7 +2,7 @@ import express from 'express';
 const router = express.Router();
 
 import SourceController from './source.controller.js';
-import upload from './source.middleware.js';
+import upload from '../../../shared/middleware/soure.middleware.js';
 import requireAuth from '../../../shared/middleware/auth.middleware.js'; 
 
 router.post('/', requireAuth, upload.single('file'), SourceController.upload);
